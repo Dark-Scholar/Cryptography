93e58352807dbb9fcce8770ea5a4f25668b223ad
LukeMcCann <developer-lukemccann@outlook.com>
2024-04-03 18:29:52 +0100

    Fix spacing issue

--- casesar_cipher/main.py
+++ casesar_cipher/main.py
@@ -1,11 +1,13 @@
-
-ALPHABET = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-KEY = 0
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+KEY = 9
 
 def caesar_encrypt(plain_text):
   cipher_text = ''
   plain_text = plain_text.upper()
   for c in plain_text:
+    if c == ' ':  # Handle spaces
+      cipher_text += c
+      continue
     index = ALPHABET.find(c)
     index = (index + KEY) % len(ALPHABET)
     cipher_text = cipher_text + ALPHABET[index]
@@ -15,6 +17,9 @@ def caesar_encrypt(plain_text):
 def caesar_decrypt(cipher_text):
   plain_text = ''
   for c in cipher_text:
+    if c == ' ':  # Handle spaces
+      plain_text += c
+      continue
     index = ALPHABET.find(c)
     if index == -1:
       plain_text += c
@@ -24,10 +29,9 @@ def caesar_decrypt(cipher_text):
 
   return plain_text
 
-message = 'Hello World'
-
+encrypted_message = caesar_encrypt('Hello World')
 print('---Encrypted Message---')
-print(caesar_encrypt(message))
+print(encrypted_message)
 print(' ')
 print('---Decrypted Message---')
-print(caesar_decrypt(message))
+print(caesar_decrypt(encrypted_message))
